534eb4288e340527b64b2746fbe4ca3e744fabc1
konglarock <konglacoco@gmail.com>
2021-03-15 16:35:19 +0700

    lab

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,18 @@ import axios from 'axios'
 const baseURL = 'https://dummyapi.io/data/api'
 const Home = () => {
   return (
-      <h1>This is home page</h1>
+    <>
+    <h1>This is home page</h1>
+    <link href="post">
+      <button>goto post page</button>
+    </link>
+    <link href="GPA Cal">
+      <button>GPA Calculator</button>
+    </link>
+    <link href="Home">
+      <button>Home</button>
+    </link>
+    </>
   )
 }
 
--- pages/post/index.js
+++ pages/post/index.js
@@ -2,13 +2,41 @@ import {useEffect,useState} from 'react'
 import Link from 'next/link'
 import axios from 'axios'
 const baseURL = 'https://dummyapi.io/data/api'
-
+const password = '604f15eac1570b730341a939'
 const Posts = () => {
     const [posts,setPosts] = useState([])
-
+    useEffect(() => {
+        axios.get(baseURL+"/post", { headers: { "app-id": password }}).then((respond)=>{
+            console.log(respond)
+            setPosts(respond.data.data)
+        })
+    },[] )
     return (
         <>
-            <h1>All Posts</h1>
+        <table style= {{width:" 100%;"}}>
+        <tr align="center">
+            <td><Link href={"/homepage"}>Home</Link></td>
+            <td><Link href={"/gallery"}>Gallery</Link></td>
+            <td><Link href={"/contact"}>Contact</Link></td>
+            <td><Link href={"/gpa"}>GPACalc</Link></td>
+            <td><Link href={"/post"}>Post</Link></td>
+
+        </tr>
+        
+    </table>
+            <h1 className="bg-red-500 text-center text-4xl p-3 tracking-widest">All Posts</h1>
+            {posts.map((item)=>{
+                return <div key={item.id}>
+                    <h4>{"Post : "+item.text}</h4>
+                    <p>
+                        <Link href={"/post/"+item.id}>
+                        <img className="imgflame" src={item.image}></img>
+                        </Link>
+                        <p>{"Likes : "+item.likes}</p>  
+
+                    </p>
+                </div>
+            })}
         </>
     )
 }
